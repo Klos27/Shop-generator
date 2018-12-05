60864a853bd3768b980cb8f61a4b98ce6461e1d8
Marcin Zielinski <zielinskim25@gmail.com>
2018-12-05 19:44:11 +0100

    fix sql

--- ShopA/Generator/src/hurtownia.sql
+++ ShopA/Generator/src/hurtownia.sql
@@ -4,99 +4,99 @@ CREATE TABLE `Produkt` (
   `opis`                   varchar(1024), 
   `nazwa`                  varchar(255), 
   `cena`                   int(10), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Klient` (
-  `id`            int(10) NOT NULL AUTO_INCREMENT, 
-  `adres_id`      int(10) NOT NULL, 
-  `kontakt_id`    int(10) NOT NULL UNIQUE, 
-  `rodzaj`        varchar(255) NOT NULL, 
-  `zgody_id`      int(10) NOT NULL, 
-  `NIP`           varchar(10), 
-  `płec`          varchar(1), 
-  `rok urodzenia` int(10), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`            int(10) NOT NULL AUTO_INCREMENT,
+  `adres_id`      int(10) NOT NULL,
+  `kontakt_id`    int(10) NOT NULL UNIQUE,
+  `rodzaj`        varchar(255) NOT NULL,
+  `zgody_id`      int(10) NOT NULL,
+  `NIP`           varchar(10),
+  `płec`          varchar(1),
+  `rok urodzenia` int(10),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Kategoria` (
-  `id`    int(10) NOT NULL AUTO_INCREMENT, 
-  `nazwa` varchar(255) NOT NULL, 
-  `opis`  varchar(255), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`    int(10) NOT NULL AUTO_INCREMENT,
+  `nazwa` varchar(255) NOT NULL,
+  `opis`  varchar(255),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Producent` (
-  `id`    int(10) NOT NULL AUTO_INCREMENT, 
-  `nazwa` varchar(255) NOT NULL, 
-  `opis`  varchar(255), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`    int(10) NOT NULL AUTO_INCREMENT,
+  `nazwa` varchar(255) NOT NULL,
+  `opis`  varchar(255),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Adres` (
-  `id`           int(10) NOT NULL AUTO_INCREMENT, 
-  `ulica`        varchar(255) NOT NULL, 
-  `nr_domu`      varchar(10) NOT NULL, 
-  `miejscowosc`  varchar(255) NOT NULL, 
-  `kod_pocztowy` varchar(8) NOT NULL, 
-  `wojewodztwo`  varchar(255) NOT NULL, 
-  `kraj`         varchar(255) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`           int(10) NOT NULL AUTO_INCREMENT,
+  `ulica`        varchar(255) NOT NULL,
+  `nr_domu`      varchar(10) NOT NULL,
+  `miejscowosc`  varchar(255) NOT NULL,
+  `kod_pocztowy` varchar(8) NOT NULL,
+  `wojewodztwo`  varchar(255) NOT NULL,
+  `kraj`         varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Kontakt` (
-  `id`       int(10) NOT NULL AUTO_INCREMENT, 
-  `email`    varchar(255), 
-  `telefon1` varchar(255), 
-  `telefon2` varchar(255), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`       int(10) NOT NULL AUTO_INCREMENT,
+  `email`    varchar(255),
+  `telefon1` varchar(255),
+  `telefon2` varchar(255),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Zgody_marketingowe` (
-  `id`          int(10) NOT NULL AUTO_INCREMENT, 
-  `zgoda_sms`   tinyint(1) NOT NULL, 
-  `zgoda_tel`   tinyint(1) NOT NULL, 
-  `zgoda_email` tinyint(1) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`          int(10) NOT NULL AUTO_INCREMENT,
+  `zgoda_sms`   tinyint(1) NOT NULL,
+  `zgoda_tel`   tinyint(1) NOT NULL,
+  `zgoda_email` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Zamowienie` (
-  `id`           int(10) NOT NULL AUTO_INCREMENT, 
-  `data_godzina` timestamp NOT NULL, 
-  `klient_id`    int(10) NOT NULL, 
-  `platnosc_id`  int(10) NOT NULL, 
-  `przesylka_id` int(10) NOT NULL, 
-  `kwota`        int(10) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`           int(10) NOT NULL AUTO_INCREMENT,
+  `data_godzina` timestamp NOT NULL,
+  `klient_id`    int(10) NOT NULL,
+  `platnosc_id`  int(10) NOT NULL,
+  `przesylka_id` int(10) NOT NULL,
+  `kwota`        int(10) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Pracownik` (
-  `id`         int(10) NOT NULL AUTO_INCREMENT, 
-  `adres_id`   int(10) NOT NULL, 
-  `kontakt_id` int(10) NOT NULL, 
-  `pensja`     int(10), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`         int(10) NOT NULL AUTO_INCREMENT,
+  `adres_id`   int(10) NOT NULL,
+  `kontakt_id` int(10) NOT NULL,
+  `pensja`     int(10),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Przesylka` (
-  `id`     int(10) NOT NULL AUTO_INCREMENT, 
-  `rodzaj` varchar(255), 
-  `cena`   int(10), 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`     int(10) NOT NULL AUTO_INCREMENT,
+  `rodzaj` varchar(255),
+  `cena`   int(10),
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Pozycja` (
-  `id`            int(10) NOT NULL AUTO_INCREMENT, 
-  `produkt_id`    int(10) NOT NULL, 
-  `zamowienie_id` int(10) NOT NULL, 
-  `cena_zakupu`   int(10) NOT NULL, 
-  `podatek VAT`   int(10) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`            int(10) NOT NULL AUTO_INCREMENT,
+  `produkt_id`    int(10) NOT NULL,
+  `zamowienie_id` int(10) NOT NULL,
+  `cena_zakupu`   int(10) NOT NULL,
+  `podatek VAT`   int(10) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Platnosc` (
-  `id`           int(10) NOT NULL AUTO_INCREMENT, 
-  `data_godzina` timestamp NOT NULL, 
-  `rodzaj`       varchar(255) NOT NULL, 
-  `kwota`        int(10) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`           int(10) NOT NULL AUTO_INCREMENT,
+  `data_godzina` timestamp NOT NULL,
+  `rodzaj`       varchar(255) NOT NULL,
+  `kwota`        int(10) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Reklamacja` (
-  `id`         int(10) NOT NULL AUTO_INCREMENT, 
-  `klient_id`  int(10) NOT NULL, 
-  `produkt_id` int(10) NOT NULL, 
-  `data`       int(10) NOT NULL, 
-  `opis`       int(10) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`         int(10) NOT NULL AUTO_INCREMENT,
+  `klient_id`  int(10) NOT NULL,
+  `produkt_id` int(10) NOT NULL,
+  `data`       int(10) NOT NULL,
+  `opis`       int(10) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Promocja` (
-  `id`             int(10) NOT NULL AUTO_INCREMENT, 
-  `od`             date NOT NULL, 
-  `do`             date NOT NULL, 
-  `produkt_id`     int(10) NOT NULL, 
-  `procent_znizki` int(2) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`             int(10) NOT NULL AUTO_INCREMENT,
+  `od`             date NOT NULL,
+  `do`             date NOT NULL,
+  `produkt_id`     int(10) NOT NULL,
+  `procent_znizki` int(2) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 CREATE TABLE `Kategoria_producent` (
-  `id`           int(10) NOT NULL AUTO_INCREMENT, 
-  `kategoria_id` int(10) NOT NULL, 
-  `producent_id` int(10) NOT NULL, 
-  PRIMARY KEY (`id`)) type=InnoDB;
+  `id`           int(10) NOT NULL AUTO_INCREMENT,
+  `kategoria_id` int(10) NOT NULL,
+  `producent_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)) COLLATE='utf8_unicode_ci' ENGINE=InnoDB;
 ALTER TABLE `Klient` ADD INDEX `FKKlient765827` (`adres_id`), ADD CONSTRAINT `FKKlient765827` FOREIGN KEY (`adres_id`) REFERENCES `Adres` (`id`);
 ALTER TABLE `Klient` ADD INDEX `FKKlient592247` (`kontakt_id`), ADD CONSTRAINT `FKKlient592247` FOREIGN KEY (`kontakt_id`) REFERENCES `Kontakt` (`id`);
 ALTER TABLE `Klient` ADD INDEX `FKKlient93750` (`zgody_id`), ADD CONSTRAINT `FKKlient93750` FOREIGN KEY (`zgody_id`) REFERENCES `Zgody_marketingowe` (`id`);
